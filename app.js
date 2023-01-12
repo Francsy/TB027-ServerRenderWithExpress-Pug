19675250f572a8786ffb18e7255088e9b51f225d
Francsy <francsyludens@gmail.com>
2023-01-12 09:54:42 +0100

    Eliminada la herramienta de body parser ya que express ya puede hacer la misma función

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser');
-const { dirname } = require('path') //
 
 const app = express()
 const port = 3000
@@ -8,22 +6,28 @@ const port = 3000
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
+// app.use('/', express.static('public'));
+// app.use('/film', express.static('public'));
 app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
     res.render('home')
+    // res.status(200).send("Has mandado una petición")
 })
 
 app.get('/film/:title?', async (req, res) => {
     let response = await fetch(`https://www.omdbapi.com/?t=${req.params.title}&apikey=3bc504a3`);
     let filmData = await response.json();
-    res.render('film', { "film" : filmData}) // si no probar con {film}
+    res.status(200).render('film', { "film" : filmData}) // si no probar con {film}
+    // res.status(200).send("Has mandado una petición")
 })
 
+//extrae de la petición del formulario el input con name "title"
 app.post('/film', (req, res) => {
-    res.redirect(`/film/${req.body.title}`);
+    res.redirect(`/film/${req.body.title}`); 
+    // res.status(201).send(`Creado ${req.body.title}`)
 })
 
 app.listen(port, () => {
